c493052b3ae8633319d4e95959864e7f7b9676ff
Rakesh Rankawat <rakesh.rankawat@outlook.com>
2021-05-29 23:53:07 +0530

    Initial Commit

---
+++ frontend/src/context/contacts/ContactState.js
@@ -0,0 +1,134 @@
+import React, { useReducer } from 'react';
+import axios from 'axios';
+import ContactContext from './contactContext';
+import contactReducer from './contactReducer';
+import {
+  GET_CONTACTS,
+  ADD_CONTACT,
+  DELETE_CONTACT,
+  SET_CURRENT,
+  CLEAR_CURRENT,
+  UPDATE_CONTACT,
+  FILTER_CONTACTS,
+  CLEAR_FILTER,
+  CONTACT_ERROR,
+  CLEAR_CONTACTS,
+} from '../types';
+
+const ContactState = (props) => {
+  const initialState = {
+    contacts: null,
+    loading: true,
+    current: null,
+    filtered: null,
+    error: null,
+  };
+
+  const [state, dispatch] = useReducer(contactReducer, initialState);
+
+  // Get Contacts
+  const getContacts = async () => {
+    try {
+      const res = await axios.get('/api/contacts');
+      dispatch({ type: GET_CONTACTS, payload: res.data });
+    } catch (err) {
+      dispatch({ dispatch: CONTACT_ERROR, payload: err.response.msg });
+    }
+  };
+
+  // Add Contact
+  const addContact = async (contact) => {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+
+    try {
+      const res = await axios.post('/api/contacts', contact, config);
+      dispatch({ type: ADD_CONTACT, payload: res.data });
+    } catch (err) {
+      dispatch({ type: CONTACT_ERROR, payload: err.response.msg });
+    }
+  };
+
+  // Delete Contact
+  const deleteContact = async (_id) => {
+    try {
+      await axios.delete(`/api/contacts/${_id}`);
+      dispatch({ type: DELETE_CONTACT, payload: _id });
+    } catch (err) {
+      dispatch({ type: CONTACT_ERROR, payload: err.response.msg });
+    }
+  };
+
+  // Update Contact
+  const updateContact = async (contact) => {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+
+    try {
+      const res = await axios.put(
+        `/api/contacts/${contact._id}`,
+        contact,
+        config
+      );
+      dispatch({ type: UPDATE_CONTACT, payload: res.data });
+    } catch (err) {
+      dispatch({ type: CONTACT_ERROR, payload: err.response.msg });
+    }
+  };
+
+  // Clear Contacts
+  const clearContacts = () => {
+    dispatch({ type: CLEAR_CONTACTS });
+  };
+
+  // Set Current Contact
+  const setCurrent = (contact) => {
+    dispatch({ type: SET_CURRENT, payload: contact });
+  };
+
+  // Clear Current Contact
+  const clearCurrent = () => {
+    dispatch({ type: CLEAR_CURRENT });
+  };
+
+  // Filter Contacts
+  const filterContacts = (text) => {
+    dispatch({ type: FILTER_CONTACTS, payload: text });
+  };
+
+  // Clear Filter
+  const clearFilter = () => {
+    dispatch({ type: CLEAR_FILTER });
+  };
+
+  return (
+    <ContactContext.Provider
+      value={{
+        contacts: state.contacts,
+        getContacts,
+        addContact,
+        updateContact,
+        deleteContact,
+        clearContacts,
+        current: state.current,
+        setCurrent,
+        clearCurrent,
+        filtered: state.filtered,
+        filterContacts,
+        clearFilter,
+        error: state.error,
+        loading: state.loading,
+      }}
+    >
+      {props.children}
+    </ContactContext.Provider>
+  );
+};
+
+export default ContactState;
---
+++ readme.md
@@ -0,0 +1,24 @@
+<h1 align="center">Contact Keeper</h1>
+
+## Server Dependencies
+
+- npm init -y
+- express: Web framework to handle routing and stuff
+- bcryptjs: To handle hasing of password
+- jsonwebtoken: For authentication, use to access protaction on routes
+- config: For global variables
+- express-validator: To validate any body data that comingin like email, pass etc.
+- mongoose: Abstraction layer to deal with database to add, read, delete, update data
+- npm run server
+
+## Server devDependencis
+
+- nodemon: It allow us to keep watching server
+- concurrently: It allow us to run backend and the frontend react at the same time
+
+## Client Dependencies
+
+- axios: To request api's
+- react-router-dom: Http router
+- uuid: To generate random id's
+- react-transition-group: For animation
